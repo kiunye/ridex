24c0291251e9793a31ede43b0023da2f340f0289
Chris <kchrismucheke@gmail.com>
2025-08-07 12:16:52 +0300

    add OS monitoring

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Ridex.MixProject do
   def application do
     [
       mod: {Ridex.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
